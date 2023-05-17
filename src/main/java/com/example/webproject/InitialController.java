4b87bdafd9731e65ceeb5a2da371d51931521f3f
david34Y <dgonzalesn@pucp.edu.pe>
2023-05-17 12:25:32 -0500

    frontend

---
+++ src/main/java/com/example/webproject/InitialController.java
@@ -0,0 +1,64 @@
+package com.example.webproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InitialController {
+    @GetMapping("/about")
+    public String acercade(){
+        return "about";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "blog";
+    }
+
+    @GetMapping("/cart")
+    public String carrito(){
+        return "cart";
+    }
+
+    @GetMapping("/checkout")
+    public String compra(){
+        return "checkout";
+    }
+
+    @GetMapping("/contact")
+    public String contacto(){
+        return "contact";
+    }
+
+    @GetMapping("/index")
+    public String indice(){
+        return "index";
+    }
+
+    @GetMapping("/portfolio")
+    public String portafolio(){
+        return "portfolio";
+    }
+
+    @GetMapping("/shop")
+    public String tienda(){
+        return "shop";
+    }
+
+    @GetMapping("/shop-details")
+    public String detallestienda(){
+        return "shop-details";
+    }
+
+    @GetMapping("/single-portfolio")
+    public String portafoliosolo(){
+        return "single-portfolio";
+    }
+
+    @GetMapping("/single-post")
+    public String postunico(){
+        return "single-post";
+    }
+
+
+}
