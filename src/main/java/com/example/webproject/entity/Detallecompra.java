1a0cea6c305d3abe644368a7252e0374a3932751
FrankLaquise <a20175947@pucp.edu.pe>
2023-07-16 16:43:29 -0500

    viaa

--- src/main/java/com/example/webproject/entity/Detallecompra.java
+++ src/main/java/com/example/webproject/entity/Detallecompra.java
@@ -1,24 +1,65 @@
 package com.example.webproject.entity;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name="detallecompra")
 public class Detallecompra {
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "iddetallecompra")
     private int iddetallecompra;
 
+    @Basic
+    @Column(name = "cantidad")
     private int cantidad;
-
-    private double preciocompra;
-
+    @Basic
+    @Column(name = "preciocompra")
+    private Double preciocompra;
     @ManyToOne
-    @JoinColumn(name = "PlantasID")
+    @JoinColumn(name = "plantas_idplantas")
     private Plantas plantas;
-
     @ManyToOne
-    @JoinColumn(name = "CompraID")
+    @JoinColumn(name = "compra_idcompra")
     private Compra compra;
 
+
+    public int getIddetallecompra() {
+        return iddetallecompra;
+    }
+
+    public void setIddetallecompra(int iddetallecompra) {
+        this.iddetallecompra = iddetallecompra;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPreciocompra() {
+        return preciocompra;
+    }
+
+    public void setPreciocompra(Double preciocompra) {
+        this.preciocompra = preciocompra;
+    }
+
+    public Plantas getPlantas() {
+        return plantas;
+    }
+
+    public void setPlantas(Plantas plantas) {
+        this.plantas = plantas;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;}
 
 }
